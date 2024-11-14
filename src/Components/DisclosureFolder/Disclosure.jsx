5caac2c028cb5692943b7934222d3731b20ad201
Brett Baker <GitBaBB@MyBaBB.com>
2024-11-14 12:18:34 -0800

    adj.

--- src/Components/DisclosureFolder/Disclosure.jsx
+++ src/Components/DisclosureFolder/Disclosure.jsx
@@ -5,9 +5,10 @@ import { FaRegHandScissors } from "react-icons/fa";
 import { FaRegHandshake } from "react-icons/fa";
 import { LuRewind } from "react-icons/lu";
 import  Copyright from "../Copyright" 
+ 
 const Disclosure = () => {
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 h-screen w-screen text-4xl">
+    <div className="absolute bottom-0 left-0 right-0 top-0 h-screen w-screen text-4xl overflow-y-auto overflow-x-hidden">
       <div className="AffiliateDisclosureHeader relative flex bg-gradient-to-b from-slate-400 via-white to-slate-400 p-4 text-black shadow-2xl">
         <h1 className="affiliateDisclosure m-auto text-center font-Kingthings_Petrock text-4xl sm:text-8xl">
           <u>Affiliate Disclosure</u>{" "}
@@ -15,14 +16,16 @@ const Disclosure = () => {
       </div>
 <div className="relative  ">
            <button
-            className="returnButton absolute left-4 top-4 z-50 rounded-full  px-8 py-2 font-DMSerifDisplay-Regular"
+            className="returnButton absolute left-4 top-4 z-50 rounded-full  px-8 py-2 font-DMSerifDisplay-Regular " 
             onClick={() => window.history.back()}>
             <LuRewind  size={24}/>
           </button>
 </div>
 
-      <section className="AffiliateDisclosureBody relative flex-col h-full items-center justify-center border-2 border-solid border-slate-900 bg-gradient-to-b from-slate-400 via-white to-slate-400 p-4 font-LibreBaskerville-Bold text-black shadow-xl">
-        <p className="affiliateParagraph m-auto mt-[8.9rem] h-fit w-[80%] rounded-2xl bg-gradient-to-b from-slate-300 via-white to-slate-300 p-8 text-center sm:text-4xl">
+      <section className="AffiliateDisclosureBody relative flex-col h-screen items-center justify-center 
+      border-y-2 border-solid border-slate-500/50 bg-gradient-to-b from-slate-400 via-white to-slate-400 p-4 font-LibreBaskerville-Bold text-black shadow-xl">
+        <p className="affiliateParagraph m-auto mt-[8.9rem] h-fit w-[80%] rounded-2xl bg-gradient-to-b
+         from-slate-300 via-white to-slate-300 p-8 text-center sm:text-4xl">
           <span className="mb-4 inline-block">
             <span className="inline-block pr-4">
               <FaRegHandScissors size={27} className="text-slate-500" />
@@ -40,8 +43,13 @@ const Disclosure = () => {
           <br />
           commission for helping.
           <hr className="m-auto mb-4 h-[4.1px] w-[20%] rounded-xl border-[1.5px] border-slate-400 bg-slate-100" />
-          I appreciate your support.
-          <br />
+          
+          
+         
+        
+           It doesn&apos;t cost you anything.
+           <br />
+           <hr className="m-auto mb-4 h-[4.1px] w-[20%] rounded-xl border-[1.5px] border-slate-400 bg-slate-100" />
           Thank you!
          
           
